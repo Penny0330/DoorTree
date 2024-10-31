5c3cb8b1c0d1e060bbc94abb7637512b470acd4d
Penny <penny860330@gmail.com>
2024-10-31 15:10:56 +0800

    FEAT: add dashboard page & create new link modal

---
+++ pages/Dashboard/LinkModal.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { X, LoaderCircle } from 'lucide-vue-next'
+
+const props = defineProps({
+  showCreateLinkModal: {
+    type: Boolean,
+    default: false,
+  },
+  isCreateLoading: {
+    type: Boolean,
+    default: false,
+  },
+  onToggleCreateLinkModal: {
+    type: Function,
+    default: () => {
+      console.warn('onToggleCreateLinkModal not provided')
+    },
+  },
+  onCreateLink: {
+    type: Function,
+    default: () => {
+      console.warn('onCreateLink not provided')
+    },
+  },
+})
+
+const link = ref<string>('')
+const domain = ref<string>('door-tree/')
+
+const isCreateDisabled = computed(() => !link.value)
+
+const onToggleCreateLinkModal = (_event: MouseEvent) => {
+  props.onToggleCreateLinkModal()
+}
+
+const onCreateLink = (_event: MouseEvent, link: string) => {
+  props.onCreateLink(link)
+}
+</script>
+
+<template>
+  <div class="w-full h-dvh fixed top-0 z-30 bg-main-overlay">
+    <div
+      class="w-11/12 bg-white rounded-2xl m-auto mt-[50%] py-4 px-5 relative sm:mt-[10%] sm:max-w-[500px]"
+      @click.stop
+    >
+      <header class="text-xl text-center my-8">
+        Create Your Custom Share Link
+      </header>
+      <X
+        class="absolute top-3 right-4 stroke-gray-400 cursor-pointer"
+        @click="onToggleCreateLinkModal"
+      />
+      <section>
+        <div class="flex items-center border rounded-md overflow-hidden">
+          <span class="pl-3 text-gray-500 cursor-pointer text-nowrap">
+            {{ domain }}
+          </span>
+          <input
+            v-model="link"
+            type="text"
+            class="flex-1 px-3 py-2"
+            placeholder="Enter a custom link name"
+          />
+        </div>
+      </section>
+      <footer class="flex justify-end gap-4 mt-8">
+        <button
+          class="btn btn-hollow btn-outline-default cursor-pointer"
+          @click="onToggleCreateLinkModal"
+        >
+          cancel
+        </button>
+        <button
+          :disabled="isCreateDisabled || isCreateLoading"
+          class="btn btn-solid btn-bg-main-blue w-[78px] cursor-pointer"
+          @click="onCreateLink($event, link)"
+        >
+          <LoaderCircle
+            v-if="isCreateLoading"
+            class="animate-spin h-4 w-4 m-auto"
+          />
+          <p v-else>create</p>
+        </button>
+      </footer>
+    </div>
+  </div>
+</template>
---
+++ pages/Dashboard/index.vue
@@ -0,0 +1,172 @@
+<script setup lang="ts">
+import { nanoid } from 'nanoid'
+import dayjs from 'dayjs'
+import { CirclePlus, Pencil, Share2, Trash2, DoorOpen } from 'lucide-vue-next'
+import LinkModal from './LinkModal.vue'
+import type { DashboardItem } from '~/types/DashboardType'
+import { useFirestore } from '@/composables/useFirestore'
+
+// import { useShowGlobalToast } from '@/composables/useGlobalToast'
+
+const store = useStore()
+const {
+  setDocument,
+  getDocument,
+  updateDocumentArray,
+  deleteDocument,
+  deleteDocumentArray,
+  // getDocumentByLink,
+} = useFirestore()
+// const showGlobalToast = useShowGlobalToast()
+
+definePageMeta({
+  middleware: 'auth',
+})
+
+const dashboardList = ref<DashboardItem[]>([])
+const isGetLoading = ref<boolean>(true)
+const showCreateLinkModal = ref<boolean>(false)
+const isCreateLoading = ref<boolean>(false)
+
+// get data
+const getDashboardData = async (): Promise<void> => {
+  const data = await getDocument('dashboard', store.uid as string)
+  dashboardList.value = data?.dashboardList as DashboardItem[]
+  isGetLoading.value = false
+}
+
+// 用 link 取得 id
+// const getShareData = async () => {
+//   const data = await getDocumentByLink('doorItemDetail', 'cat')
+//   console.log('data: ', data)
+// }
+
+// create
+const onShowCreateLinkModal = () => {
+  showCreateLinkModal.value = true
+}
+
+const onToggleCreateLinkModal = () => {
+  showCreateLinkModal.value = !showCreateLinkModal.value
+}
+
+const onCreateLink = async (link: string) => {
+  isCreateLoading.value = true
+  const newDashboardItem = {
+    id: nanoid(),
+    title: link,
+    link,
+    updateTime: dayjs().format('YYYY/MM/DD HH:mm'),
+  }
+
+  await updateDocumentArray(
+    'dashboard',
+    store.uid as string,
+    'dashboardList',
+    newDashboardItem,
+  )
+  createDetail(newDashboardItem.id, link)
+  isCreateLoading.value = false
+  onToggleCreateLinkModal()
+  navigateTo(`/edit/${newDashboardItem.id}`)
+}
+
+const createDetail = async (itemId: string, link: string) => {
+  const newItem = {
+    id: itemId,
+    title: link,
+    link,
+  }
+  await setDocument('doorItemDetail', itemId, newItem)
+}
+
+// go to edit page
+const onEdit = (id: string) => {
+  navigateTo(`/edit/${id}`)
+}
+
+// go to share page
+const onShareLink = (link: string) => {
+  navigateTo(`/share/${link}`)
+}
+
+// delete
+const onDelete = async (removeItem: DashboardItem) => {
+  await deleteDocumentArray(
+    'dashboard',
+    store.uid as string,
+    'dashboardList',
+    removeItem,
+  )
+  await deleteDocument('doorItemDetail', removeItem.id)
+  getDashboardData()
+}
+
+onMounted(() => {
+  getDashboardData()
+})
+</script>
+
+<template>
+  <div
+    class="bg-white pt-24 pb-8 min-h-[calc(100vh-5rem)] flex flex-col items-center"
+  >
+    <section
+      class="flex flex-col items-center flex-wrap gap-4 w-11/12 sm:flex-row sm:items-stretch"
+    >
+      <div
+        v-if="!isGetLoading"
+        class="bg-main-blue text-white w-full p-4 rounded flex justify-center items-center my-6 cursor-pointer sm:basis-[calc(50%-1rem)] sm:my-0 sm:text-2xl xl:basis-[calc(33.33%-1rem)]"
+        @click="onShowCreateLinkModal"
+      >
+        <DoorOpen class="stroke-white mr-4 sm:w-8 sm:h-8" />
+        Create New
+      </div>
+      <div
+        v-for="item in dashboardList"
+        :key="item.id"
+        class="border rounded w-full p-4 flex justify-center items-center gap-8 cursor-pointer sm:basis-[calc(50%-1rem)] xl:basis-[calc(33.33%-1rem)] xl:py-8"
+        @click="onEdit(item.id)"
+      >
+        <img
+          src="https://fakeimg.pl/100x100/"
+          class="rounded-full w-4/12 h-1/3 sm:h-auto"
+        />
+        <div>
+          <p class="text-xl">{{ item.title }}</p>
+          <p class="text-sm text-gray-400">updated：{{ item.updateTime }}</p>
+          <div class="mt-4 flex flex-col gap-2">
+            <div class="flex items-center gap-2">
+              <Pencil class="w-4 h-4 stroke-gray-500" />
+              <span>Edit</span>
+            </div>
+            <div
+              class="flex items-center gap-2"
+              @click="onShareLink(item.link)"
+            >
+              <Share2 class="w-4 h-4 stroke-gray-500" />
+              <span>Share Link</span>
+            </div>
+            <div class="flex items-center gap-2" @click.stop="onDelete(item)">
+              <Trash2 class="w-4 h-4 stroke-gray-500" />
+              <span>Delete</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="fixed bottom-12 right-4 bg-main-blue rounded-full">
+      <CirclePlus
+        class="w-10 h-10 stroke-white cursor-pointer"
+        @click="onShowCreateLinkModal"
+      />
+    </div>
+
+    <LinkModal
+      v-if="showCreateLinkModal"
+      :is-create-loading="isCreateLoading"
+      :on-toggle-create-link-modal="onToggleCreateLinkModal"
+      :on-create-link="onCreateLink"
+    />
+  </div>
+</template>
